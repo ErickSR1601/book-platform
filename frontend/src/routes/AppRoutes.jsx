73de9b821a4c915e95b6b591e3b87ae4de10d7b6
Erick <dev.ericksr.16@gmail.com>
2025-08-14 20:49:50 -0600

    Add ProtectedRoute for authenticated routes
    Introduced a ProtectedRoute component to restrict access to certain routes based on user authentication. Updated AppRoutes to wrap dashboard and book management routes with ProtectedRoute, redirecting unauthenticated users to the login page.

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -5,20 +5,38 @@ import RegisterUser from "../pages/RegisterUser";
 import Login from "../pages/Login";
 import RecoverPassword from "../pages/RecoverPassword";
 import EditBook from "../pages/EditBook";
+import ProtectedRoute from "./ProtectedRoute";
 
 function AppRoutes() {
   return (
     <Router>
       <Routes>
-        {/* Dashboard: lista de libros */}
-        <Route path="/" element={<Dashboard />} />
-        {/* CRUD de libros */}
-        <Route path="/books/new" element={<RegisterBook />} /> 
-        <Route path="/books/:id/edit" element={<EditBook />} />
-        {/* Usuarios */}
-        <Route path="/users/new" element={<RegisterUser />} /> 
-        {/* Autenticación */}
-        <Route path="/login" element={<Login />} /> 
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/new"
+          element={
+            <ProtectedRoute>
+              <RegisterBook />
+            </ProtectedRoute>
+          }
+        />
+        <Route
+          path="/books/:id/edit"
+          element={
+            <ProtectedRoute>
+              <EditBook />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="/users/new" element={<RegisterUser />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/password/recover" element={<RecoverPassword />} />
       </Routes>
     </Router>
