b4d7dcdd9f89287a955c750c49cee51e243242eb
Erick Solis <dev.ericksr.16@gmail.com>
2025-08-28 18:49:22 -0600

    Update AuthController.js

--- backend/controllers/AuthController.js
+++ backend/controllers/AuthController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
-const generateToken = require("../utils/generateToken");
+const generateToken = require("../utils/GenerateToken");
 
 // POST /api/auth/register
 const registerUser = async (req, res) => {
